9a666c21d2c6484fc2dd8496ad040431d28a1f4a
Todd H. Gardner <todd@wocketware.com>
2018-04-14 17:29:51 -0500

    added new click and visibility analytics

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -3,7 +3,7 @@
   * This is the main execution script for PubConf event pages.
   */
 
-/* global $, ga, util */
+/* global $, gtag, util */
 
 //= require jquery
 //= require jquery.magnific
@@ -75,6 +75,60 @@ $(function() {
     });
   })();
 
+  (function attachClickAnalytics() {
+    var categoryAttr = 'data-click-category';
+    var actionAttr = 'data-click-action';
+    var labelAttr = 'data-click-label';
+    var elements = util.makeArray(document.querySelectorAll('[' + actionAttr + ']'));
+    util.forEach(elements, function(element) {
+      element.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        var category = element.getAttribute(categoryAttr) || '';
+        var action = element.getAttribute(actionAttr) || '';
+        var label = element.getAttribute(labelAttr) || '';
+        gtag('event', action, {
+          'event_category': category,
+          'event_label': label,
+          'transport_type': 'beacon',
+          'event_callback': gtag.createTimeoutFn(function() {
+            if (element.tagName === 'A') {
+              document.location = element.getAttribute('href');
+            }
+          })
+        });
+      });
+    });
+  })();
+
+  (function attachVisibleAnalytics() {
+    var categoryAttr = 'data-visible-category';
+    var actionAttr = 'data-visible-action';
+    var labelAttr = 'data-visible-label';
+    var visibleEls = util.makeArray(document.querySelectorAll('[' + actionAttr + ']'));
+    if (!visibleEls.length) { return; }
+
+    window.addEventListener('scroll', (function() {
+      var timeout;
+      return function throttledOnScroll() {
+        timeout = timeout || setTimeout(function() {
+          util.forEach(visibleEls, function(el) {
+            if (util.elementIsVisible(el) && el.hasAttribute(actionAttr)) {
+              var category = el.getAttribute(categoryAttr) || '';
+              var action = el.getAttribute(actionAttr) || '';
+              var label = el.getAttribute(labelAttr) || '';
+              gtag('event', action, {
+                'event_category': category,
+                'event_label': label,
+                'transport_type': 'beacon'
+              });
+              el.removeAttribute(actionAttr);
+            }
+          });
+          timeout = null;
+        }, 250);
+      };
+    })());
+  })();
 
   /**
     * lazyLoadImages
@@ -99,74 +153,3 @@ $(function() {
   })();
 
 });
-
-(function(document, window, undefined) {                  // eslint-disable-line
-
-  if (!document.querySelector || !window.addEventListener) { return; }
-
-
-  /**
-    * addVisibleElementAnalytics
-    * IIFE that will find annotated elements and attach event listeners for
-    * when the element is visible in the viewport.
-    *
-    * `data-visible-event="{category}"` will send ga event {category}:visible:{eventId}
-    */
-  (function addVisibleElementAnalytics() {
-    var attribute = 'data-visible-event';
-
-    var visibleEls = util.makeArray(document.querySelectorAll('[' + attribute + ']'));
-    if (!visibleEls.length) { return; }
-
-    window.addEventListener('scroll', (function() {
-      var timeout;
-
-      return function throttledOnScroll() {
-        timeout = timeout || setTimeout(function() {
-          util.forEach(visibleEls, function(el) {
-            if (util.elementIsVisible(el) && el.hasAttribute(attribute)) {
-              var category = el.getAttribute(attribute);
-              var event = {
-                'hitType': 'event',
-                'eventCategory': category,
-                'eventAction': 'visible'
-              };
-              console.info('send element visible event', el, event);
-              ga('send', event);
-              el.removeAttribute(attribute);
-            }
-          });
-          timeout = null;
-        }, 250);
-      };
-    })());
-
-  })();
-
-  /**
-    * addClickElementAnalytics
-    * IIFE that will find annotated elements and attach event listeners for
-    * when the element is clicked.
-    *
-    * `data-click-event="{category}"` will send ga event {category}:click:{eventId}
-    */
-  (function addClickElementAnalytics() {
-    var attribute = 'data-click-event';
-
-    util.forEach(document.querySelectorAll('[' + attribute + ']'), function(el) {
-      el.addEventListener('click', function() {
-        var category = el.getAttribute(attribute);
-        var event = {
-          'hitType': 'event',
-          'eventCategory': category,
-          'eventAction': 'click'
-        };
-        console.info('send element click event', el, event);
-        ga('send', event);
-        el.removeAttribute(attribute);
-      });
-    });
-
-  })();
-
-})(document, window);
